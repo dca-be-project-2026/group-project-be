a1110655cfcdbd5550721b71f997697c97436d9c
Yazan Kassouaa <yazan.kassouaa@gmail.com>
2026-05-20 20:34:58 +0200

    implement get all boards and git board by id

--- routes/boards.js
+++ routes/boards.js
@@ -1,12 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send({ data: 'Here are your boards.' });
+  const boards = prisma.board.findMany();
+  res.json(boards);
 });
 
 router.get('/:id', (req, res) => {
-  res.send({ data: 'Here is your requested board.' });
+  const id = req.params.id;
+  prisma.board
+    .findUnique({
+      where: { id: id },
+    })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ error: `Board with id ${id} is not found` });
+        return;
+      }
+      console.log('test');
+      res.json(data);
+    })
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 router.post('/', (req, res) => {
